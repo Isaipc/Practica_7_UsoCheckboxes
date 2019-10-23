1b0b07a79e82ca184245b055e39c48441ff035e0
M4S4RU <isai.polito6052@gmail.com>
2019-10-23 03:35:49 -0500

    Nueva Activity

--- app/src/main/java/com/app/practica_7_usocheckboxes/Main6Activity.java
+++ app/src/main/java/com/app/practica_7_usocheckboxes/Main6Activity.java
@@ -3,12 +3,51 @@ package com.app.practica_7_usocheckboxes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
-public class Main6Activity extends AppCompatActivity {
+public class Main6Activity extends AppCompatActivity
+    implements CompoundButton.OnCheckedChangeListener
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main6);
+
+        ((CheckBox)findViewById(R.id.check_tv)).setOnCheckedChangeListener(this);
+        ((CheckBox)findViewById(R.id.check_radio)).setOnCheckedChangeListener(this);
+        ((CheckBox)findViewById(R.id.check_periodico)).setOnCheckedChangeListener(this);
+        ((CheckBox)findViewById(R.id.check_amigo)).setOnCheckedChangeListener(this);
+        ((CheckBox)findViewById(R.id.check_otros)).setOnCheckedChangeListener(this);
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        switch (buttonView.getId())
+        {
+            case R.id.check_tv:
+                findViewById(R.id.et_tv)
+                        .setVisibility((isChecked ? View.VISIBLE: View.GONE));
+                break;
+            case R.id.check_radio:
+                findViewById(R.id.et_radio)
+                        .setVisibility((isChecked ? View.VISIBLE: View.GONE));
+                break;
+            case R.id.check_periodico:
+                findViewById(R.id.et_periodico)
+                        .setVisibility((isChecked ? View.VISIBLE: View.GONE));
+                break;
+            case R.id.check_amigo:
+                findViewById(R.id.et_amigo)
+                        .setVisibility((isChecked ? View.VISIBLE: View.GONE));
+                break;
+            case R.id.check_otros:
+                findViewById(R.id.et_otros)
+                        .setVisibility((isChecked ? View.VISIBLE: View.GONE));
+                break;
+        }
+
     }
 }
